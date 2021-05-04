7fd45ff405a648bcf7bda99bb37ac5ec69b90e1b
ayoungblood2 <60159318+ayoungblood2@users.noreply.github.com>
2021-05-04 09:31:06 -0500

    c

---
+++ .history/docs/src/utils_20210503235538.js
@@ -0,0 +1,8 @@
+export function lerp(current, target, speed = 0.1, limit = 0.001) {
+    let change = (target - current) * speed;
+    if (Math.abs(change) < limit) {
+      change = target - current;
+    }
+    return change;
+  }
+  
